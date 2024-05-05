156730d929db401aecfba946521b44d6827c48b2
hazzazabdul <hazzazabdulmannan@gmail.com>
2024-05-06 05:57:20 +0600

    clearInterval problem solved

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
-//! Problem 01: https://leetcode.com/problems/timeout-cancellation/?envType=study-plan-v2&envId=30-days-of-javascript
+//! Problem 01: Timeout Cancellation
 
 /**
  * Solution 01:
  */
 
-const cancellable = function (fn, args, t) {
-  const timer = setTimeout(() => fn(...args), t);
+// const cancellable = function (fn, args, t) {
+//   const timer = setTimeout(() => fn(...args), t);
 
-  return function () {
-    clearTimeout(timer);
-  };
+//   return function () {
+//     clearTimeout(timer);
+//   };
+// };
+
+//! Problem 02: Interval Cancellation
+
+/**
+ * Solution: 02
+ */
+
+const cancellable = function (fn, args, t) {
+  fn(...args);
+  const interval = setInterval(() => fn(...args), t);
+  return () => clearInterval(interval);
 };
+
